f1eeb6be4161ced4e1aeb8a5e78f3093a4659379
Shangfeng Ding <747879583@qq.com>
2019-06-18 23:26:05 +0800

    hardcore

--- workspace-gateway/src/main/java/com/aliware/tianchi/UserLoadBalance.java
+++ workspace-gateway/src/main/java/com/aliware/tianchi/UserLoadBalance.java
@@ -30,199 +30,25 @@ import java.util.concurrent.atomic.AtomicLong;
  */
 public class UserLoadBalance extends AbstractLoadBalance implements LoadBalance {
 
-//    private static AtomicInteger count = new AtomicInteger(0);
-//
-//    @Override
-//    public <T> Invoker<T> select(List<Invoker<T>> invokers, URL url, Invocation invocation) throws RpcException {
+    private static AtomicInteger count = new AtomicInteger(0);
+
+    @Override
+    public <T> Invoker<T> select(List<Invoker<T>> invokers, URL url, Invocation invocation) throws RpcException {
 //        int small = invokers.get(0).getUrl().getParameter(Constants.THREADS_KEY, Constants.DEFAULT_THREADS);
 //        int medium = invokers.get(1).getUrl().getParameter(Constants.THREADS_KEY, Constants.DEFAULT_THREADS);
 //        int large = invokers.get(2).getUrl().getParameter(Constants.THREADS_KEY, Constants.DEFAULT_THREADS);
-//        int typeCount = count.getAndAdd(1);
-//        typeCount%=6;
-//        switch (typeCount){
-//            case 0:
-//                return invokers.get(0);
-//            case 1:
-//            case 2:
-//                return invokers.get(1);
-//            default:
-//                return invokers.get(2);
-//        }
-////        return invokers.get(ThreadLocalRandom.current().nextInt(invokers.size()));
-//    }
-    public static final String NAME = "roundrobin";
-
-    private static final int RECYCLE_PERIOD = 60000;
-
-    protected static class WeightedRoundRobin {
-        private int weight;
-        private AtomicLong current = new AtomicLong(0);
-        private long lastUpdate;
-        public int getWeight() {
-            return weight;
+        int typeCount = count.getAndAdd(1);
+        typeCount%=6;
+        switch (typeCount){
+            case 0:
+                return invokers.get(0);
+            case 1:
+            case 2:
+                return invokers.get(1);
+            default:
+                return invokers.get(2);
         }
-        public void setWeight(int weight) {
-            this.weight = weight;
-            current.set(0);
-        }
-        public long increaseCurrent() {
-            return current.addAndGet(weight);
-        }
-        public void sel(int total) {
-            current.addAndGet(-1 * total);
-        }
-        public long getLastUpdate() {
-            return lastUpdate;
-        }
-        public void setLastUpdate(long lastUpdate) {
-            this.lastUpdate = lastUpdate;
-        }
+//        return invokers.get(ThreadLocalRandom.current().nextInt(invokers.size()));
     }
 
-    private ConcurrentMap<String, ConcurrentMap<String, UserLoadBalance.WeightedRoundRobin>> methodWeightMap = new ConcurrentHashMap<String, ConcurrentMap<String, UserLoadBalance.WeightedRoundRobin>>();
-    private AtomicBoolean updateLock = new AtomicBoolean();
-
-    /**
-     * get invoker addr list cached for specified invocation
-     * <p>
-     * <b>for unit test only</b>
-     *
-     * @param invokers
-     * @param invocation
-     * @return
-     */
-    protected <T> Collection<String> getInvokerAddrList(List<Invoker<T>> invokers, Invocation invocation) {
-        String key = invokers.get(0).getUrl().getServiceKey() + "." + invocation.getMethodName();
-        Map<String, UserLoadBalance.WeightedRoundRobin> map = methodWeightMap.get(key);
-        if (map != null) {
-            return map.keySet();
-        }
-        return null;
-    }
-
-    @Override
-    public  <T> Invoker<T> select(List<Invoker<T>> invokers, URL url, Invocation invocation) {
-        String key = invokers.get(0).getUrl().getServiceKey() + "." + invocation.getMethodName();
-        ConcurrentMap<String, UserLoadBalance.WeightedRoundRobin> map = methodWeightMap.get(key);
-        if (map == null) {
-            methodWeightMap.putIfAbsent(key, new ConcurrentHashMap<String, UserLoadBalance.WeightedRoundRobin>());
-            map = methodWeightMap.get(key);
-        }
-        int totalWeight = 0;
-        long maxCurrent = Long.MIN_VALUE;
-        long now = System.currentTimeMillis();
-        Invoker<T> selectedInvoker = null;
-        UserLoadBalance.WeightedRoundRobin selectedWRR = null;
-        for (Invoker<T> invoker : invokers) {
-            String identifyString = invoker.getUrl().toIdentityString();
-            UserLoadBalance.WeightedRoundRobin weightedRoundRobin = map.get(identifyString);
-            int weight = getWeight(invoker, invocation);
-
-            if (weightedRoundRobin == null) {
-                weightedRoundRobin = new UserLoadBalance.WeightedRoundRobin();
-                weightedRoundRobin.setWeight(weight);
-                map.putIfAbsent(identifyString, weightedRoundRobin);
-            }
-            if (weight != weightedRoundRobin.getWeight()) {
-                //weight changed
-                weightedRoundRobin.setWeight(weight);
-            }
-            long cur = weightedRoundRobin.increaseCurrent();
-            weightedRoundRobin.setLastUpdate(now);
-            if (cur > maxCurrent) {
-                maxCurrent = cur;
-                selectedInvoker = invoker;
-                selectedWRR = weightedRoundRobin;
-            }
-            totalWeight += weight;
-        }
-        if (!updateLock.get() && invokers.size() != map.size()) {
-            if (updateLock.compareAndSet(false, true)) {
-                try {
-                    // copy -> modify -> update reference
-                    ConcurrentMap<String, UserLoadBalance.WeightedRoundRobin> newMap = new ConcurrentHashMap<String, UserLoadBalance.WeightedRoundRobin>();
-                    newMap.putAll(map);
-                    Iterator<Map.Entry<String, UserLoadBalance.WeightedRoundRobin>> it = newMap.entrySet().iterator();
-                    while (it.hasNext()) {
-                        Map.Entry<String, UserLoadBalance.WeightedRoundRobin> item = it.next();
-                        if (now - item.getValue().getLastUpdate() > RECYCLE_PERIOD) {
-                            it.remove();
-                        }
-                    }
-                    methodWeightMap.put(key, newMap);
-                } finally {
-                    updateLock.set(false);
-                }
-            }
-        }
-        if (selectedInvoker != null) {
-            selectedWRR.sel(totalWeight);
-            return selectedInvoker;
-        }
-        // should not happen here
-        return invokers.get(0);
-    }
-
-    @Override
-    protected <T> Invoker<T> doSelect(List<Invoker<T>> invokers, URL url, Invocation invocation) {
-        String key = invokers.get(0).getUrl().getServiceKey() + "." + invocation.getMethodName();
-        ConcurrentMap<String, UserLoadBalance.WeightedRoundRobin> map = methodWeightMap.get(key);
-        if (map == null) {
-            methodWeightMap.putIfAbsent(key, new ConcurrentHashMap<String, UserLoadBalance.WeightedRoundRobin>());
-            map = methodWeightMap.get(key);
-        }
-        int totalWeight = 0;
-        long maxCurrent = Long.MIN_VALUE;
-        long now = System.currentTimeMillis();
-        Invoker<T> selectedInvoker = null;
-        UserLoadBalance.WeightedRoundRobin selectedWRR = null;
-        for (Invoker<T> invoker : invokers) {
-            String identifyString = invoker.getUrl().toIdentityString();
-            UserLoadBalance.WeightedRoundRobin weightedRoundRobin = map.get(identifyString);
-            int weight = getWeight(invoker, invocation);
-
-            if (weightedRoundRobin == null) {
-                weightedRoundRobin = new UserLoadBalance.WeightedRoundRobin();
-                weightedRoundRobin.setWeight(weight);
-                map.putIfAbsent(identifyString, weightedRoundRobin);
-            }
-            if (weight != weightedRoundRobin.getWeight()) {
-                //weight changed
-                weightedRoundRobin.setWeight(weight);
-            }
-            long cur = weightedRoundRobin.increaseCurrent();
-            weightedRoundRobin.setLastUpdate(now);
-            if (cur > maxCurrent) {
-                maxCurrent = cur;
-                selectedInvoker = invoker;
-                selectedWRR = weightedRoundRobin;
-            }
-            totalWeight += weight;
-        }
-        if (!updateLock.get() && invokers.size() != map.size()) {
-            if (updateLock.compareAndSet(false, true)) {
-                try {
-                    // copy -> modify -> update reference
-                    ConcurrentMap<String, UserLoadBalance.WeightedRoundRobin> newMap = new ConcurrentHashMap<String, UserLoadBalance.WeightedRoundRobin>();
-                    newMap.putAll(map);
-                    Iterator<Map.Entry<String, UserLoadBalance.WeightedRoundRobin>> it = newMap.entrySet().iterator();
-                    while (it.hasNext()) {
-                        Map.Entry<String, UserLoadBalance.WeightedRoundRobin> item = it.next();
-                        if (now - item.getValue().getLastUpdate() > RECYCLE_PERIOD) {
-                            it.remove();
-                        }
-                    }
-                    methodWeightMap.put(key, newMap);
-                } finally {
-                    updateLock.set(false);
-                }
-            }
-        }
-        if (selectedInvoker != null) {
-            selectedWRR.sel(totalWeight);
-            return selectedInvoker;
-        }
-        // should not happen here
-        return invokers.get(0);
-    }
 }
